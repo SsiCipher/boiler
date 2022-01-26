1185ef6c432d80a4b13b572716dbcb6bd21cb346
SsiCipher <complexan0@gmail.com>
2022-01-26 09:49:42 -0600

    🤡 ADDED: Better comments

--- boiler.sh
+++ boiler.sh
@@ -1,147 +1,147 @@
 #!/usr/bin/bash
 
-NC='\033[0m'
-Black='\033[0;30m'
-Dark_Gray='\033[1;30m'
-Red='\033[0;31m'
-Light_Red='\033[1;31m'
-Green='\033[0;32m'
-Light_Green='\033[1;32m'
-Orange='\033[0;33m'
-Yellow='\033[1;33m'
-Blue='\033[0;34m'
-Light_Blue='\033[1;34m'
-Purple='\033[0;35m'
-Light_Purple='\033[1;35m'
-Cyan='\033[0;36m'
-Light_Cyan='\033[1;36m'
-Light_Gray='\033[0;37m'
-White='\033[1;37m'
-
-DIR="$HOME/Cursus"
+source "${0%/*}/colors.sh"
+DEFAULT_DIR="$HOME/Projects"
+GITIGNORE_TEMPLATE="${0%/*}/templates/gitignore.template"
+MAKEFILE_TEMPLATE="${0%/*}/templates/makefile.template"
+GH_PROFILE="https://github.com/SsiCipher"
 
 create_dir() {
-	if [[ -e "$DIR/$1" ]]; then
-		echo -e "📂 ${Light_Gray}Project exists already 😒${NC}"
+	if [[ -e "$DEFAULT_DIR/$1" ]]; then
+		echo -e "📂 ${LIGHT_GRAY}Project exists already 😒${NC}"
+		return 1
 	else
-		echo -e "📂 ${Light_Gray}Creating project directory in: $DIR 😎${NC}"
-		mkdir "$DIR/$1"
+		mkdir "$DEFAULT_DIR/$1"
+		echo -e "📂 ${LIGHT_GRAY}Created project directory $DEFAULT_DIR/$1${NC}\n"
+		return 0
 	fi
-	echo ""
 }
 
 init_repo() {
-	if [[ -e "$DIR/$1/.git" ]]; then
-		echo -e "🐙 ${Light_Cyan}Folder is already a Git repo${NC}"
+	if [[ -e "$DEFAULT_DIR/$1/.git" ]]; then
+		echo -e "🐙 ${BLUE}Folder is already a Git repo${NC}\n"
 	else
-		echo -e "🐙 ${Light_Cyan}Init a Git repository${NC}"
-		git init "$DIR/$1"
+		git init "$DEFAULT_DIR/$1" 1>/dev/null
+		echo -e "🐙 ${BLUE}Initialized a Git repository in $DEFAULT_DIR/$1${NC}\n"
 	fi
-	echo ""
 }
 
 add_repo_files() {
-	if [[ -e "$DIR/$1/README.md" ]]; then
-		echo -e "📄 ${Light_Purple}README.md file exists already${NC}"
+	if [[ -e "$DEFAULT_DIR/$1/README.md" ]]; then
+		echo -e "📄 ${LIGHT_PURPLE}README.md file exists already${NC}"
 	else
-		echo -e "📄 ${Light_Purple}Creating a README.md file${NC}"
-		echo "# $1" > "$DIR/$1/README.md"
 		echo -n "Project Description: "
 		read DESC
-		echo -e "\n$DESC" >> "$DIR/$1/README.md"
+		echo ""
+		echo "# $1" > "$DEFAULT_DIR/$1/README.md"
+		echo -e "\n$DESC" >> "$DEFAULT_DIR/$1/README.md"
+		echo -e "📄 ${LIGHT_PURPLE}Created README.md file${NC}"
 	fi
 
-	if [[ -e "$DIR/$1/.gitignore" ]]; then
-		echo -e "📄 ${Light_Purple}.gitignore file exists already${NC}"
+	if [[ -e "$DEFAULT_DIR/$1/.gitignore" ]]; then
+		echo -e "📄 ${LIGHT_PURPLE}.gitignore file exists already${NC}"
 	else
-		echo -e "📄 ${Light_Purple}Creating a .gitignore file${NC}"
-		cp .gitignore_template "$DIR/$1/.gitignore"
+		cp $GITIGNORE_TEMPLATE "$DEFAULT_DIR/$1/.gitignore"
+		echo -e "📄 ${LIGHT_PURPLE}Created .gitignore file${NC}"
 	fi
 	echo ""
 }
 
 create_folders() {
-	if [[ ! -e "$DIR/$1/src" ]]; then
-		mkdir "$DIR/$1/src"
+	if [[ ! -e "$DEFAULT_DIR/$1/src" ]]; then
+		mkdir "$DEFAULT_DIR/$1/src"
+		echo -e "📂 ${LIGHT_GREEN}Created 'src' folder${NC}"
 	fi
-	if [[ ! -e "$DIR/$1/includes" ]]; then
-		mkdir "$DIR/$1/includes"
-	fi
-	if [[ ! -e "$DIR/$1/libs" ]]; then
-		mkdir "$DIR/$1/libs"
-	fi
-}
 
-create_main_files() {
-	if [[ ! -e "$DIR/$1/includes/$1.h" ]]; then
-		echo "#ifndef ${1^^}_H" > "$DIR/$1/includes/$1.h"
-		echo "# define ${1^^}_H" >> "$DIR/$1/includes/$1.h"
-		echo "" >> "$DIR/$1/includes/$1.h"
-		echo "#endif" >> "$DIR/$1/includes/$1.h"
+	if [[ ! -e "$DEFAULT_DIR/$1/includes" ]]; then
+		mkdir "$DEFAULT_DIR/$1/includes"
+		echo -e "📂 ${LIGHT_GREEN}Created 'includes' folder${NC}"
 	fi
-	if [[ ! -e "$DIR/$1/$1.c" ]]; then
-		echo "#include \"includes/$1.h\"" > "$DIR/$1/$1.c"
-	fi
-}
 
-create_makefile() {
-	if [[ ! -e "$DIR/$1/Makefile" ]]; then
-		echo "" > "$DIR/$1/Makefile"
+	if [[ ! -e "$DEFAULT_DIR/$1/libs" ]]; then
+		mkdir "$DEFAULT_DIR/$1/libs"
+		echo -e "📂 ${LIGHT_GREEN}Created 'libs' folder${NC}"
 	fi
+
+	echo ""
 }
 
 clone_libs() {
-	local gh_profile="https://github.com/SsiCipher"
-
-	if [[ ! -e "$DIR/$1/libs/libft" ]]; then
-		echo -n -e "👉 Clone ${Blue}libft${NC} 🤔? [y/n]: "
+	if [[ ! -e "$DEFAULT_DIR/$1/libs/libft" ]]; then
+		echo -n -e "👉 Clone ${ORANGE}libft${NC} 🤔? [y/n]: "
 		read -n 1 libft
 		echo ""
 		if [[ $libft == 'y' ]]; then
-			echo -e "✅ ${Green}Cloning libft...${NC}"
-			git clone "$gh_profile/libft" "$DIR/$1/libs/libft"
+			git clone "$GH_PROFILE/libft" "$DEFAULT_DIR/$1/libs/libft" 2>/dev/null
+			echo -e "✅ ${GREEN}Added libft to libs folder${NC}"
 		fi
 	fi
 
-	if [[ ! -e "$DIR/$1/libs/libftprintf" ]]; then
-		echo -n -e "👉 Clone ${Blue}ft_printf${NC} 🤔? [y/n]: "
+	if [[ ! -e "$DEFAULT_DIR/$1/libs/libftprintf" ]]; then
+		echo -n -e "👉 Clone ${ORANGE}ft_printf${NC} 🤔? [y/n]: "
 		read -n 1 ft_printf
 		echo ""
-		if [[ $ft_printf == 'y' && ! -e "$DIR/$1/libs/libftprintf" ]]; then
-			echo -e "✅ ${Green}Cloning printf...${NC}"
-			git clone "$gh_profile/ft_printf" "$DIR/$1/libs/libftprintf"
+		if [[ $ft_printf == 'y' && ! -e "$DEFAULT_DIR/$1/libs/libftprintf" ]]; then
+			git clone "$GH_PROFILE/ft_printf" "$DEFAULT_DIR/$1/libs/libftprintf" 2>/dev/null
+			echo -e "✅ ${GREEN}Added ft_printf to libs folder${NC}"
 		fi
 	fi
 
-	if [[ ! -e "$DIR/$1/libs/libgnl" ]]; then
-		echo -n -e "👉 Clone ${Blue}libgnl${NC} 🤔? [y/n]: "
+	if [[ ! -e "$DEFAULT_DIR/$1/libs/libgnl" ]]; then
+		echo -n -e "👉 Clone ${ORANGE}libgnl${NC} 🤔? [y/n]: "
 		read -n 1 libgnl
 		echo ""
 		if [[ $libgnl == 'y' ]]; then
-			echo -e "✅ ${Green}Cloning libgnl...${NC}"
-			git clone "$gh_profile/get_next_line" "$DIR/$1/libs/libgnl"
+			git clone "$GH_PROFILE/get_next_line" "$DEFAULT_DIR/$1/libs/libgnl" 2>/dev/null
+			echo -e "✅ ${GREEN}Added get_next_line to libs folder${NC}"
 		fi
 	fi
 
-	find "$DIR/$1/libs" -name ".git" -exec rm -rf {} \; 2>/dev/null
-	find "$DIR/$1/libs" -name ".gitignore" -exec rm -rf {} \; 2>/dev/null
-	find "$DIR/$1/libs" -name "README.md" -exec rm -rf {} \; 2>/dev/null
+	echo ""
 
-	find "$DIR/$1/libs" -name "*.h" -exec cp {} "$DIR/$1/includes" \; 2>/dev/null
+	find "$DEFAULT_DIR/$1/libs" -name ".git" -exec rm -rf {} \; 2>/dev/null
+	find "$DEFAULT_DIR/$1/libs" -name ".gitignore" -exec rm -rf {} \; 2>/dev/null
+	find "$DEFAULT_DIR/$1/libs" -name "README.md" -exec rm -rf {} \; 2>/dev/null
+
+	find "$DEFAULT_DIR/$1/libs" -name "*.h" -exec cp {} "$DEFAULT_DIR/$1/includes" \; 2>/dev/null
+}
+
+create_main_files() {
+	if [[ ! -e "$DEFAULT_DIR/$1/includes/$1.h" ]]; then
+		echo -e "#ifndef ${1^^}_H\n# define ${1^^}\n\n$(ls $DEFAULT_DIR/$1/includes | awk '{printf("# include \"%s\"\n", $1)}')\n\n#endif" > "$DEFAULT_DIR/$1/includes/$1.h"
+	fi
+
+	if [[ ! -e "$DEFAULT_DIR/$1/$1.c" ]]; then
+		echo -n -e "${YELLOW}Main source file name (${DARK_GRAY}$1.c${YELLOW}): ${NC}"
+		read name
+		if [[ -z $name ]]; then
+			echo "#include \"includes/$1.h\"" > "$DEFAULT_DIR/$1/$1.c"
+		else
+			echo "#include \"includes/$1.h\"" > "$DEFAULT_DIR/$1/$name.c"
+		fi
+	fi
+
 	echo ""
 }
 
+create_makefile() {
+	if [[ ! -e "$DEFAULT_DIR/$1/Makefile" ]]; then
+		cp $MAKEFILE_TEMPLATE "$DEFAULT_DIR/$1/Makefile"
+	fi
+}
+
 if [[ -z $1 ]]; then
-	echo -e "✨ ${Red}😡 Enter a project name!!!${NC}"
+	echo -e "✨ ${RED}😡 Enter a project name!!!${NC}"
 else
-	echo "✨ Creating project [>$1<] in $DIR 🥳"
+	echo -e "✨ ${GRAY}Creating project '$1' in $DEFAULT_DIR 🥳${NC}"
 	echo ""
-	create_dir $1
-	init_repo $1
-	add_repo_files $1
-	create_folders $1
-	create_main_files $1
-	create_makefile $1
-	clone_libs $1
-	echo "👍 Project Created successfully, Happy coding"
+	if create_dir $1; then
+		init_repo $1
+		add_repo_files $1
+		create_folders $1
+		clone_libs $1
+		create_main_files $1
+		create_makefile $1
+		echo -e "👍 ${GRAY}Project Created successfully, Happy coding${NC}"
+	fi
 fi
