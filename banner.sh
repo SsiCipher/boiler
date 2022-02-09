1deabc8d1272765ea31e064a76c2ea2401046e3f
SsiCipher <94566747+SsiCipher@users.noreply.github.com>
2022-02-09 03:39:33 +0100

    🚧 ADDED : banner file for boiler

---
+++ banner.sh
@@ -0,0 +1,17 @@
+NC='\033[0m'
+DARK_GRAY='\033[1;30m'
+LIGHT_GREEN='\033[1;32m'
+LIGHT_BLUE='\033[1;34m'
+
+echo ""
+echo -e "${DARK_GRAY} \t Welcome to ${NC}"
+echo -e "${LIGHT_BLUE} \t  ____   ____  _____ _    _____ ____ ${NC}"
+echo -e "${LIGHT_BLUE} \t | *_* )/    \|_   _| |  | ____|  _ \\ ${NC}"
+echo -e "${LIGHT_BLUE} \t |  _  \  /\  \ | | | |  |  _| | |_) | ${NC}"
+echo -e "${LIGHT_BLUE} \t | |_)  | \/  /_| |_| |__| |___|  _ < ${NC}"
+echo -e "${LIGHT_BLUE} \t |_____/\____/______|____|_______| \\_\\ ${NC}"
+echo ""
+echo -e "${LIGHT_BLUE} \t Version \t: ${NC} 1.0.0"
+echo -e "${LIGHT_BLUE} \t Author  \t: ${NC} Youssef An (Aka: SsiCipher)"
+echo -e "${LIGHT_BLUE} \t Github  \t: ${NC} SsiCipher"
+echo ""
